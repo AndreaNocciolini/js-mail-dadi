6d7b1317c612a81067ad2441236413de126467da
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-24 16:21:55 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -3,11 +3,27 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-const mailArray = [`chiara@gmail.com`, `acquarius@gmail.com`, `andrea@yahoo.com`, `rossi-ste99@gmail.com`, `fuleio69@alice.it`, `xxdarksoldierxx@virgilio.it`]
-console.log(mailArray);
-console.log(mailArray.length);
+const mailArray = [`chiara@gmail.com`, `acquarius@gmail.com`, `andrea@yahoo.com`, `rossi-ste99@gmail.com`, `fuleio69@alice.it`, `xxdarksoldierxx@virgilio.it`];
+const validityText = document.querySelector(`.validity-text`);
+const mail = prompt(`Insert your email`);
+const html = document.querySelector(`html`);
+
+let validity = false;
 
 
-// Gioco dei dadi
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+for (let i = 0; i < mailArray.length; i++) {
+    const element = mailArray[i];
+    
+    if (mail == element) {
+        validity = true;
+    }
+}
+
+if (validity == true) {
+    validityText.innerHTML = `Welcome ${mail}!`;
+    html.classList.add(`validity-true`);
+}
+else if (validity == false) {
+    validityText.innerHTML = `You're not on the list.`;
+    html.classList.add(`validity-false`);
+}
