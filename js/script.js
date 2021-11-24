6adab8cbaf67522f08ee3d459afef197c3e802c7
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-11-24 16:07:06 +0100

    re-start

--- js/script.js
+++ js/script.js
@@ -7,15 +7,7 @@ const mailArray = [`chiara@gmail.com`, `acquarius@gmail.com`, `andrea@yahoo.com`
 console.log(mailArray);
 console.log(mailArray.length);
 
-const mail = document.getElementById(`insert-mail`);
-const mailButton = document.getElementById(`mail-button`);
-console.log(mail)
-console.log(mailButton)
 
-for (let i = 0; i < mailArray.length; i++) {
-    const element = mailArray[i];
-    console.log(element);
-}
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
